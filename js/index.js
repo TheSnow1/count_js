8cd5ca1807c8aca17724dba33ad58709680c8c94
Snow <101468907+TheSnow1@users.noreply.github.com>
2023-01-26 16:26:36 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+let plusNode = document.querySelector(`#plus`);
+let minusNode = document.querySelector(`#minus`);
+let amountNode = document.querySelector(`#amount`);
+let count = 0;
+
+plusNode.addEventListener(`click`, function () {
+    count = count + 1;
+    amountNode.innerHTML = count;
+});
+
+minusNode.addEventListener(`click`, function () {
+    count = count - 1;
+    amountNode.innerHTML = count;
+});
